c1a83cff690a1abc08965df18ba2843da9faba43
unknown <silaciok@gmail.com>
2019-03-07 10:36:59 +0200

    Homework

---
+++ index.js
@@ -0,0 +1,36 @@
+var browser = prompt("Please enter the day");
+
+switch(day) {
+    case "Monday":
+      alert("Working day");
+        break;
+
+    case "Tuesday":
+      alert("Working day");
+        break;
+
+    case "Wednesday":
+      alert("Working day");
+        break;
+        
+    case "Thursday":
+      alert("Working day");    
+        break;
+        
+    case "Friday":
+      alert("Working day");
+        break;
+        
+    case "Saturday":
+      alert("Weekend");
+        break;
+        
+    case "Sunday":
+      alert("Weekend");
+        break;
+        
+    default:
+      alert("Can not define, please try another value");    
+        break;
+}
+
